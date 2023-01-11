97341ff785a501944ffd539f225d3c4a2b22600e
vanessa-liang <118566123+vanessa-liang@users.noreply.github.com>
2023-01-11 13:19:31 -0800

    base for app

--- my-redux-app/src/App.js
+++ my-redux-app/src/App.js
@@ -1,56 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import {useEffect, useState} from 'react';
+import './globals/globalVariables';
+import { posterPath } from './globals/globalVariables';
 
 function App() {
+  const [moviesData, setMoviesData] = useState('');
+
+  const fetchData = async () => {
+    const results = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=5d25ff38c62c8743cafcfe4221c1f5ae&language=en-US&page=1');
+    const jsonData = await results.json();
+    setMoviesData( jsonData.results);
+    }
+
+    useEffect(() => {
+      fetchData();
+    }, []);
+  
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      {moviesData === '' ? (
+        '<h1>Fetching todos...</h1>'
+       ) : (
+         <ul>
+           {moviesData.map(item => (
+              <>
+              <img key={item.id} src={posterPath+item.poster_path}></img>
+              <li key={item.id}>{item.title}</li>
+             </>
+           ))}
+        </ul>
+      )}
     </div>
   );
 }
