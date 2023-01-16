7ee86a3953e4fea54e58b12952b1ceb9408b0354
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-01-16 10:54:08 -0800

    fix router

--- my-redux-app/src/routers/AppRouters.js
+++ my-redux-app/src/routers/AppRouters.js
@@ -1,25 +1,23 @@
-import { BrowserRouter, Routes, Route } from “react-router-dom”;
-import Header from “../components/Header”;
-import Nav from “../components/Nav”;
-import Footer from “../components/Footer”;
-import PageHome from “../pages/PageHome”;
-import PageManageUser from “../pages/PageManageUser”;
-import PageCreateUser from “../pages/PageCreateUser”;
-import PageContact from “../pages/PageContact”;
-import PageNotFound from “../pages/PageNotFound”;
-import { appTitle } from “../globals/globalVariables”;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import PageHome from "../pages/Home";
+import PageAbout from "../pages/About";
+import PageSingle from "../pages/Single";
+import PageFavourite from "../pages/Favourite";
+import PageNotFound from "../pages/Page-not-found";
+
 function AppRouter() {
   return (
     <BrowserRouter>
-      <div className=“wrapper”>
-        <Header title={appTitle} />
+      <div className="nav-wrapper">
         <Nav />
         <Routes>
-          <Route path=“/” exact element={<PageHome />} />
-          <Route path=“/manage-user” element={<PageManageUser />} />
-          <Route path=“/create-user” element={<PageCreateUser />} />
-          <Route path=“/contact” element={<PageContact />} />
-          <Route path=“*” element={<PageNotFound />} />
+          <Route path="/" exact element={<PageHome />} />
+          <Route path="/about" element={<PageAbout />} />
+          <Route path="/favourite" element={<PageFavourite />} />
+          <Route path="/single" element={<PageSingle />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
         <Footer />
       </div>
@@ -27,14 +25,3 @@ function AppRouter() {
   );
 }
 export default AppRouter;
-
-
-
-
-
-
-
-
-
-
-
