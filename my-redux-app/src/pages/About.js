26c0ff91ee2b04d4b8dbba8eaa809c8428183a35
Lily Colby <117856062+lilycolby@users.noreply.github.com>
2023-01-23 09:39:22 -0800

    about content and css

--- my-redux-app/src/pages/About.js
+++ my-redux-app/src/pages/About.js
@@ -3,7 +3,17 @@ import React from "react";
 function About() {
   return (
     <div>
-      <div>hi</div>
+      <div className="about-title">
+        <h1>About</h1>
+      </div>
+      <div className="about-content">
+       <h3> WOW movies! we love em. So we 
+        made a data base where you can 
+        stream and watch YOUR favourite 
+        movies. So make some popcorn 
+        grab a drink and get ready 
+        to LIVE, LAUGH, and LOVE!</h3>
+      </div>
     </div>
   );
 }
