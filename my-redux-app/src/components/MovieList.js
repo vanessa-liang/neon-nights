5c2b8994904003b7298f87eaff422692f01cd67a
Sam Screpnek <118566100+samscrepnek@users.noreply.github.com>
2023-01-26 15:12:13 -0800

    fixed some stuff

--- my-redux-app/src/components/MovieList.js
+++ my-redux-app/src/components/MovieList.js
@@ -9,10 +9,12 @@ const MovieList = (props) => {
     <>
       {props.movies.map((movie) => (
         <div key={movie.id} className="image-container d-flex justify-content-start m-3">
-          <FavBtn />
-          <Link key={movie.id} to={`/Single/${movie.id}`}>
-            <img src={posterPath + movie.poster_path} alt="movie"></img>
-          </Link>
+          <div className="movie-fav">
+            <FavBtn />
+            <Link key={movie.id} to={`/Single/${movie.id}`}>
+              <img src={posterPath + movie.poster_path} alt="movie"></img>
+            </Link>
+          </div>
         </div>
       ))}
     </>
