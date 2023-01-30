045e53d4c45c469d8329aac7ca9d14d2111bf32b
Lily Colby <117856062+lilycolby@users.noreply.github.com>
2023-01-30 14:14:32 -0800

    changes to about

--- my-redux-app/src/pages/About.js
+++ my-redux-app/src/pages/About.js
@@ -3,17 +3,14 @@ import React from "react";
 function About() {
   return (
     <div>
-      <div className="about-title">
-        <h1>About</h1>
-      </div>
       <div className="about-content">
-       <h3> WOW movies! we love em. So we 
-        made a data base where you can 
-        stream and watch YOUR favourite 
-        movies. So make some popcorn 
-        grab a drink and get ready 
-        to LIVE, LAUGH, and LOVE!</h3>
+       <p> This product uses the TMDb API but is not endorsed or certified by TMDb.
+            The “Movie Database” is a community built movie and TV database that allows you to search, favorite, and find more information on movies and TV shows around the world.
+            This application was created for educational purposes only.</p>
       </div>
+
+      <img src="/src/images/themoviedb.svg" alt="TMDB logo"></img>
+      
     </div>
   );
 }
