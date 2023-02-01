cc11a3022956e786e351cc2023ad153599fa284f
Lily Colby <117856062+lilycolby@users.noreply.github.com>
2023-01-31 16:29:35 -0800

    Update About.js

--- my-redux-app/src/pages/About.js
+++ my-redux-app/src/pages/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <div>
       <div className="about-content">
-       <p> This product uses the TMDb API but is not endorsed or certified by TMDb.
+       <p> This product uses the TMDB API but is not endorsed or certified by TMDB.
             The “Movie Database” is a community built movie and TV database that allows you to search, favorite, and find more information on movies and TV shows around the world.
             This application was created for educational purposes only.</p>
       </div>
