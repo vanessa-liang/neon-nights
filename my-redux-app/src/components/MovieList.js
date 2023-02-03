7846f04e13f56bf712f57e8705d89448f32b0894
vanessa-liang <118566123+vanessa-liang@users.noreply.github.com>
2023-02-03 12:53:46 -0800

    updated hover

--- my-redux-app/src/components/MovieList.js
+++ my-redux-app/src/components/MovieList.js
@@ -18,6 +18,13 @@ const MovieList = (props) => {
     dispatch(deleteMovie(mov));
   };
 
+  function truncateText(text, chars = 150) {
+    let newText = text.substring(0, 150);
+    const lastSpace = newText.lastIndexOf(' ');
+    newText = newText.substring(0,lastSpace);
+      return newText;
+  }
+
   return (
     <>
       {props.movies.map((movie) => (
@@ -35,7 +42,8 @@ const MovieList = (props) => {
                 handleAddMov={handleAddMov}
                 handleDeleteMov={handleDeleteMov}
                 mov={movie}/>
-                <p className="overview">{`${movie.overview.substring(0, 150)}...`}</p>
+                {/* <p className="overview">{`${movie.overview.substring(0, 150)}...`}</p> */}
+                <p className="overview">{`${truncateText(movie.overview, 150)}...`}</p>
                 <p className="date">{movie.release_date}</p>
                 <p className="rating">{`${movie.vote_average * 10}%`}</p>
                 <a href={`/Single/${movie.id}`}>
